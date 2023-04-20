d762b143322fe47015a52b61cbc32ddd0b218bd2
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-04-20 17:54:33 +0530

    Updating functioning of site
    Removing bugs and activating the sliding motion of the pages of the website

--- js/index.js
+++ js/index.js
@@ -5,29 +5,34 @@ var typed = new Typed(".typing", {
     loop: true
 })
 
-const nav = document.querySelector(".nav"), navList = nav.querySelectorAll("li"), totalNavList = navList.length;
-
-allSection = document.querySelectorAll(".section"), totalSection = allSection.length;
-
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
 for (let i = 0; i < totalNavList; i++) {
     const a = navList[i].querySelector("a");
     a.addEventListener("click", function () {
         removeBackSection();
         for (let j = 0; j < totalNavList; j++) {
-            if (navList[j].querySelector("a").classList.contains("active"))
+
+            if (navList[j].querySelector("a").classList.contains("active")) {
                 addBackSection(j);
+            }
             navList[j].querySelector("a").classList.remove("active");
         }
         this.classList.add("active")
         showSection(this);
-        if (window.innerWidth < 1200)
+        if (window.innerWidth < 1200) {
             asideSectionTogglerBtn();
+        }
     })
 }
 
 function removeBackSection() {
-    for (let i = 0; i < totalSection; i++)
+    for (let i = 0; i < totalSection; i++) {
         allSection[i].classList.remove("back-section");
+    }
 }
 
 function addBackSection(num) {
@@ -35,8 +40,9 @@ function addBackSection(num) {
 }
 
 function showSection(element) {
-    for (let i = 0; i < totalSection; i++)
+    for (let i = 0; i < totalSection; i++) {
         allSection[i].classList.remove("active");
+    }
     const target = element.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active")
 }
@@ -45,8 +51,9 @@ function updateNav(element) {
     for (let i = 0; i < totalNavList; i++) {
         navList[i].querySelector("a").classList.remove("active");
         const target = element.getAttribute("href").split("#")[1];
-        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
             navList[i].querySelector("a").classList.add("active");
+        }
     }
 }
 
@@ -59,8 +66,8 @@ document.querySelector(".hire-me").addEventListener("click", function () {
     addBackSection(sectionIndex);
 })
 
-const navTogglerBtn = document.querySelector(".nav-toggler"), aside = document.querySelector(".aside");
-
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
 navTogglerBtn.addEventListener("click", () => {
     asideSectionTogglerBtn();
 })
@@ -68,6 +75,7 @@ navTogglerBtn.addEventListener("click", () => {
 function asideSectionTogglerBtn() {
     aside.classList.toggle("open");
     navTogglerBtn.classList.toggle("open");
-    for (let i = 0; i < totalSection; i++)
+    for (let i = 0; i < totalSection; i++) {
         allSection[i].classList.toggle("open");
+    }
 }
--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -5,23 +5,25 @@ styleSwitcherToggle.addEventListener("click", () => {
 })
 
 window.addEventListener("scroll", () => {
-    if (document.querySelector(".style-switcher").classList.contains("open"))
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
         document.querySelector(".style-switcher").classList.remove("open");
+    }
 })
 
 const alternateStyles = document.querySelectorAll(".alternate-style");
 
 function setActiveStyle(color) {
     alternateStyles.forEach((style) => {
-        if (color == style.getAttribute("title"))
+        if (color == style.getAttribute("title")) {
             style.removeAttribute("disabled");
-        else
+        }
+        else {
             style.setAttribute("disabled", "true");
+        }
     })
 }
 
 const dayNight = document.querySelector(".day-night");
-
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
@@ -29,8 +31,10 @@ dayNight.addEventListener("click", () => {
 })
 
 window.addEventListener("load", () => {
-    if (document.body.classList.contains("dark"))
+    if (document.body.classList.contains("dark")) {
         dayNight.querySelector("i").classList.add("fa-sun");
-    else
+    }
+    else {
         dayNight.querySelector("i").classList.add("fa-moon");
+    }
 })
